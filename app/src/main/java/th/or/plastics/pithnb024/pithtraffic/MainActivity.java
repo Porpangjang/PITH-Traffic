71d100879fefe1051615346c38246da32578f94a
pithnb024 <wanwisa.s@thaiplastics.org>
2015-11-19 15:56:39 +0700

    Day1 Finish

--- app/src/main/java/th/or/plastics/pithnb024/pithtraffic/MainActivity.java
+++ app/src/main/java/th/or/plastics/pithnb024/pithtraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package th.or.plastics.pithnb024.pithtraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -17,12 +21,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Manager ( Bind widget เป็นการผูก widget ต้องทำเสมอ ผูกกับตัวแปร)
-
-
         bindWidget();// ใส่วงเล็บตัวแปร
 
+        //ไว้ใช้ควบคุมปุ่ม Button Controller
+        buttonController();
 
     }//Main method end ;
+
+    private void buttonController() { //OnClickListener กดคลิกแล้วทำงานเลย
+         aboutMEButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) { //(View v)(ประเภทตัวแปร ตัวแปร)
+
+                 //sound
+                 MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.dog); //getBaseContext อยู่ที่เดียวกับมัน
+                 buttonMediaPlayer.start();
+                 //internet to webview
+                 Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                 objIntent.setData(Uri.parse("https://youtu.be/Jy35oa5oFoY"));
+                 startActivity(objIntent);
+             }// event
+         });
+    }
 
     private void bindWidget() {
 
---
+++ app/src/main/java/th/or/plastics/pithnb024/pithtraffic/MyAdapter.java
@@ -0,0 +1,10 @@
+package th.or.plastics.pithnb024.pithtraffic;
+
+/**
+ * Created by pithnb024 on 19/11/2558.
+ */
+public class MyAdapter {
+
+
+
+}// Main Class
