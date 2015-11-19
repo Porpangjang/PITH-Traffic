c319bb9ac136f74d68ad0079ba155d464bdd7302
pithnb024 <wanwisa.s@thaiplastics.org>
2015-11-19 14:21:09 +0700

    Bind Widget เสร้จแล้ว

--- app/src/main/java/th/or/plastics/pithnb024/pithtraffic/MainActivity.java
+++ app/src/main/java/th/or/plastics/pithnb024/pithtraffic/MainActivity.java
@@ -2,12 +2,33 @@ package th.or.plastics.pithnb024.pithtraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //พื้นที่ที่ไว้ให้การศตัวแปร EXPLICIT
+    private Button aboutMEButton;
+    private ListView trafficListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Manager ( Bind widget เป็นการผูก widget ต้องทำเสมอ ผูกกับตัวแปร)
+
+
+        bindWidget();// ใส่วงเล็บตัวแปร
+
+
+    }//Main method end ;
+
+    private void bindWidget() {
+
+        aboutMEButton = (Button) findViewById(R.id.button);//เป็นการผูกตัวแปรเข้าหากัน
+        trafficListView = (ListView) findViewById(R.id.listView);
+
     }
-}
+
+} //Main Class
